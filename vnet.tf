96aeeff1833ce610c4722ae2e435637799f429a4
Aidarova93 <104389538+Aidarova93@users.noreply.github.com>
2022-08-09 11:43:17 -0500

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "wordpress" {
 }
 
 resource "azurerm_subnet" "wordpress" {
-  name                 = "wordpress-subnet"
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
   resource_group_name  = azurerm_resource_group.wordpress.name
   virtual_network_name = azurerm_virtual_network.wordpress.name
   address_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
