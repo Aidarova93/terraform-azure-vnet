c2758dec5ed231a287cdbce7708cb22883badf54
Aidarova93 <104389538+Aidarova93@users.noreply.github.com>
2022-08-09 11:57:18 -0500

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,23 +1,27 @@
-resource "azurerm_virtual_network" "wordpress" {
-  name                = "wordpress-vnet"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "Team2" {
+  name     = "my-resources"
+  location = "West Europe"
+}
+
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.Team2.name
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.wordpress.name
-  tags                = var.tags
-}
-
-resource "azurerm_subnet" "wordpress" {
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
-  resource_group_name  = azurerm_resource_group.wordpress.name
-  virtual_network_name = azurerm_virtual_network.wordpress.name
-  address_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
 
-resource "azurerm_public_ip" "wordpress" {
-  name                = "wordpress-public-ip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.wordpress.name
-  allocation_method   = "Static"
-  domain_name_label   = random_string.fqdn.result
-  tags                = var.tags
+  subnet_service_endpoints = {
+    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
+    subnet3 = ["Microsoft.AzureActiveDirectory"]
+  }
+
+  tags = {
+    environment = "DevOps"
+  }
+
+  depends_on = [azurerm_resource_group.Team2]
 }
